9514c544e118d30426c6b245a4d960da6e08e34d
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-09-23 23:01:50 +0600

    feat(color): add `env` command config

--- colorize.zsh
+++ colorize.zsh
@@ -5,7 +5,7 @@ if ! tty -s || [ ! -n "$TERM" ] || [ "$TERM" = dumb ] || (( ! $+commands[coloriz
 fi
 
 cmds=(
-    ping stat traceroute df
+    ping stat traceroute df env
     cp mv rm ps lsblk mount
 )
 
