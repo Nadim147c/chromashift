9f25f02b833d3fac0398739deefc6d1c2ee46e8b
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-09-23 17:09:12 +0600

    feat(alias): add `zsh` function alias script for available cmds

---
+++ colorize.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+if ! tty -s || [ ! -n "$TERM" ] || [ "$TERM" = dumb ] || (( ! $+commands[colorize] )); then
+    return
+fi
+
+cmds=(
+    ping stat traceroute df
+    cp mv rm ps lsblk
+)
+
+for cmd in $cmds ; do
+    if (( $+commands[$cmd] )) ; then
+        $cmd() {
+            colorize -- ${commands[$0]} "$@"
+        }
+    fi
+done
+
+unset cmds cmd
