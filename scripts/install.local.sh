5449a96aa8dbf64039dfaf83d4353cb0036e4641
Ephemeral <theephemeral.txt@gmail.com>
2024-09-26 23:39:25 +0600

    chore(config)!: remove colorize.toml to config.toml
    Breaking Changes: remove colorize.toml to config.toml

--- scripts/install.local.sh
+++ scripts/install.local.sh
@@ -5,5 +5,5 @@ DESTDIR="${PREFIX}/etc/colorize"
 mkdir -p "$DESTDIR/rules"
 
 install -m 644 rules/* "$DESTDIR/rules"
-install -m 644 "colorize.toml" "$DESTDIR/config.toml"
-install -m 644 "colorize.zsh" "$DESTDIR"
+install -m 644 "config.toml" "$DESTDIR/config.toml"
+install -m 644 "scripts/alias.zsh" "$DESTDIR"
