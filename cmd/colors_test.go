854cf07f7fef3b74829eef175a44095ab6cd332b
Ephemeral <theephemeral.txt@gmail.com>
2024-10-06 19:00:06 +0600

    test(GetColor): add tests for `GetColor`

---
+++ cmd/colors_test.go
@@ -0,0 +1,43 @@
+package cmd_test
+
+import (
+	"colorize/cmd"
+	"testing"
+)
+
+func TestGetColor(t *testing.T) {
+	t.Run("Get color", func(t *testing.T) {
+		ansiColor := cmd.Ansi.GetColor("red")
+		if ansiColor != cmd.Ansi.Red {
+			t.Fatalf("expected %s, but got %s", printAnsi(cmd.Ansi.Red), printAnsi(ansiColor))
+		}
+	})
+
+	t.Run("Get color with all upper case", func(t *testing.T) {
+		ansiColor := cmd.Ansi.GetColor("CYAN")
+		if ansiColor != cmd.Ansi.Cyan {
+			t.Fatalf("expected %s, but got %s", printAnsi(cmd.Ansi.Cyan), printAnsi(ansiColor))
+		}
+	})
+
+	t.Run("Get color with ambiguous case", func(t *testing.T) {
+		ansiColor := cmd.Ansi.GetColor("bLUe")
+		if ansiColor != cmd.Ansi.Blue {
+			t.Fatalf("expected %s, but got %s", printAnsi(cmd.Ansi.Blue), printAnsi(ansiColor))
+		}
+	})
+
+	t.Run("Get color with ambiguous text", func(t *testing.T) {
+		ansiColor := cmd.Ansi.GetColor(" GrEEn   ")
+		if ansiColor != cmd.Ansi.Green {
+			t.Fatalf("expected %s, but got %s", printAnsi(cmd.Ansi.Green), printAnsi(ansiColor))
+		}
+	})
+
+	t.Run("Get color empty on invalid_color", func(t *testing.T) {
+		ansiColor := cmd.Ansi.GetColor("invalid_color")
+		if ansiColor != "" {
+			t.Fatalf("expected %s, but got %s", "Empty String", printAnsi(ansiColor))
+		}
+	})
+}
